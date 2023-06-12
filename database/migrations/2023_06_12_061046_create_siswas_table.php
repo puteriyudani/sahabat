45976fe5966060743680b12585eb7932ff257a9f
Puteri Yudani <puteriyudani@gmail.com>
2023-06-12 13:27:34 +0700

    tabel siswa

---
+++ database/migrations/2023_06_12_061046_create_siswas_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('siswas', function (Blueprint $table) {
+            $table->id();
+            $table->integer('orangtua_id');
+            $table->string('nama');
+            $table->string('panggilan');
+            $table->bigInteger('noinduk');
+            $table->string('kelompok');
+            $table->string('tempat_lahir');
+            $table->date('tanggal_lahir');
+            $table->string('jenis_kelamin');
+            $table->string('agama');
+            $table->string('anakke');
+            $table->string('nama_ayah');
+            $table->string('nama_ibu');
+            $table->string('alamat');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('siswas');
+    }
+};
